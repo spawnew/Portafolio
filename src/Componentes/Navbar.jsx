04d99f2d7a0a3997f63739142e3b67eba131b9aa
Lucas Sorzio <sorziolucas4@gmail.com>
2025-07-16 20:48:32 -0300

    empezando el navbar

---
+++ src/Componentes/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+      <>
+      
+          <ul>
+              <li> <button >Contacto</button></li>
+              <li> <button>Proyectos</button></li>
+              <li> <button>Sobre mi</button></li>
+              <li> <button>Contacto</button></li>
+              <li> <button>Contacto</button></li>
+      </ul>
+      
+      
+      
+      </>
+  )
+}
+
+export default Navbar
