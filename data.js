c9f1cd08ae21518aaac5af5194065ab102dac447
Lucas Sorzio <sorziolucas4@gmail.com>
2025-07-30 22:51:33 -0300

    agregado de algunos proyectos

--- data.js
+++ data.js
@@ -1,6 +1,9 @@
 import lista from './src/assets/lista.PNG'
 import imperio from './src/assets/imperio.JPG'
 import buscador from './src/assets/buscador.JPG'
+import pokemon from './src/assets/pokemon.JPG'
+import rick from './src/assets/rick.JPG'
+import calculadora from './src/assets/calculadora.JPG'
 const proyecto = [
     {
         id:1,
@@ -31,11 +34,29 @@ const proyecto = [
     },
     {
         id:4,
-      title: "Buscador Gift",
-      description: "Busca imagenes que coincidan con lo ingresado ",
-      image: buscador,
-      tecnologia: ["React ", " JavaScript ", " Tailwind "],
-      url: "https://patitas-t-pitschool-d1ey.vercel.app/",
+      title: "Pokedex",
+      description: "Muestra una lista de pokemones su tipo y ataques ",
+      image: pokemon,
+      tecnologia: ["React ", " JavaScript ","Css"],
+      url: "https://pokeapi-ac77a27vw-lucas-projects-5937df71.vercel.app/",
+      githubUrl: "#"
+    },
+    {
+        id:5,
+      title: "Rick and Morty",
+      description: "Muestra una lista de personajes de la serie Rick and Morty tambien te muestra el estado y el detalle de cada personaje",
+      image: rick,
+      tecnologia: ["React ", " JavaScript ","Css"],
+      url: "https://rick-and-morty-a3hx.vercel.app/",
+      githubUrl: "#"
+    },
+    {
+        id:6,
+      title: "Calculadora",
+      description: "Calculadora simple que permite realizar operaciones basicas",
+      image: calculadora,
+      tecnologia: ["React ", " JavaScript ","Css"],
+      url:" https://calculadora-60d1vhy8k-lucas-projects-5937df71.vercel.app/",
       githubUrl: "#"
     },
 ]
