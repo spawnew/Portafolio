651d3a6ccf9203868ae995ff528084906f3637a1
Lucas Sorzio <sorziolucas4@gmail.com>
2025-08-01 14:21:06 -0300

    l

--- src/Componentes/Proyecto/ProyectoCard.jsx
+++ src/Componentes/Proyecto/ProyectoCard.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { motion } from 'framer-motion'
 const ProyectoCard = ({titulo,descripcion,imagen,url,tecnologia}) => {
   return (
-      <motion.div {{ transition: { duration: 0.3 } }} 
+      <motion.div initial={{scale:1}}
      className='p-2 bg-black border border-blue-800 rounded-lg shadow-md flex flex-col justify-center items-center hover:scale-105 o hover:shadow-lg w-90'>
           <h2 className='text-2xl font-bold p-1 text-gray-500'> {titulo}</h2>
           <img src={imagen} className='w-80 h-50 p-1 border-1 border-blue-700' alt="foto del proyecto"  />
