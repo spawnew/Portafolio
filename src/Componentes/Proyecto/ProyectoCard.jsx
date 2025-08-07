0c09987c53c357eefe1c95653f4a6e856c6304e2
Lucas Sorzio <sorziolucas4@gmail.com>
2025-08-07 20:02:52 -0300

    cambio de stilos

--- src/Componentes/Proyecto/ProyectoCard.jsx
+++ src/Componentes/Proyecto/ProyectoCard.jsx
@@ -8,7 +8,7 @@ const ProyectoCard = ({titulo,descripcion,imagen,url,tecnologia}) => {
             viewport={{ once: true, amount: 0.4 }}
                 
      className='p-2 bg-black border border-blue-800 rounded-lg shadow-md flex flex-col justify-center items-center hover:scale-105 o hover:shadow-lg w-90'>
-          <h2 className='text-2xl font-bold p-1 text-gray-500'> {titulo}</h2>
+          <h2 className='text-2xl font-bold p-1 text-gray-300'> {titulo}</h2>
           <img src={imagen} className='w-80 h-50 p-1 border-1 border-blue-700' alt="foto del proyecto"  />
           <p className='m-3 text-amber-50 '>{descripcion}</p>
           <p className='text-amber-50 '>{tecnologia}</p>
