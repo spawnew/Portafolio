243f87a02cd42f0ae7d5230a2b6e9f0193a509bc
Lucas Sorzio <sorziolucas4@gmail.com>
2025-08-07 19:27:16 -0300

    Arreglo del contendor de los proyectos

--- src/Componentes/Proyecto/ProyectoCard.jsx
+++ src/Componentes/Proyecto/ProyectoCard.jsx
@@ -10,7 +10,7 @@ const ProyectoCard = ({titulo,descripcion,imagen,url,tecnologia}) => {
      className='p-2 bg-black border border-blue-800 rounded-lg shadow-md flex flex-col justify-center items-center hover:scale-105 o hover:shadow-lg w-90'>
           <h2 className='text-2xl font-bold p-1 text-gray-500'> {titulo}</h2>
           <img src={imagen} className='w-80 h-50 p-1 border-1 border-blue-700' alt="foto del proyecto"  />
-          <p className='text-amber-50 p-1'>{descripcion}</p>
+          <p className='m-3 text-amber-50 '>{descripcion}</p>
           <p className='text-amber-50 '>{tecnologia}</p>
           <a href={url} className='text-blue-500 hover:underline'>Ver Proyecto</a>
 
